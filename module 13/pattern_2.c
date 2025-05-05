35f7ba2f6179d53efcaad4cd8d330bc83b4663a0
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2025-05-05 09:52:33 +0600

    solve two pattern

--- module 13/pattern_2.c
+++ module 13/pattern_2.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, k=1;
     scanf("%d", &n);
-    
-    int k = n;
-    
+        
     for(int i =1; i<=n; i++){
          for(int j=1; j<=k; j++){
              printf("* ");
          }
-         k--;
+         k++;
         printf("\n");
     }
 
     return 0;
 }
 
-
 // output
 
-// * * * * 
-// * * * 
-// * * 
 // * 
+// * * 
+// * * * 
+// * * * * 
